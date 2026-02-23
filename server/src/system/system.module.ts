b0bab355aeece65f855fd8f90c63b760ea606b47
Jonas Ribeiro <suporte-noc@metelecom.com.br>
2026-02-22 23:17:23 -0300

    fix: restore missing SystemController import in SystemModule

--- server/src/system/system.module.ts
+++ server/src/system/system.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { SystemController } from './system.controller';
 import { SuperAdminController } from './super-admin.controller';
 import { TenantsModule } from '../tenants/tenants.module';
 import { AuthModule } from '../auth/auth.module';
